6df17980bd3b97e2b4e284b07d43fc61c729a45c
Jenifer <jeniferprochnow@web.de>
2023-01-11 14:23:56 +0100

    fix repos

--- src/main/java/com/jeniferprochnow/facebookcloneservice/FacebookCloneServiceApplication.java
+++ src/main/java/com/jeniferprochnow/facebookcloneservice/FacebookCloneServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackages = {"com.jeniferprochnow.facebookcloneservice"})
+@EnableJpaRepositories(basePackages = {"com.jeniferprochnow.facebookcloneservice.repository"})
 @EntityScan(basePackages = {"com.jeniferprochnow.facebookcloneservice.entity"})
 public class FacebookCloneServiceApplication {
 
