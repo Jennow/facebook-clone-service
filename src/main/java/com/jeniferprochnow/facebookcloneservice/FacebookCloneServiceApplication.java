43ee767b67c7f793363969fd16b97bf5199ddec6
Jenifer <jeniferprochnow@web.de>
2023-01-11 14:17:52 +0100

    add anotations

--- src/main/java/com/jeniferprochnow/facebookcloneservice/FacebookCloneServiceApplication.java
+++ src/main/java/com/jeniferprochnow/facebookcloneservice/FacebookCloneServiceApplication.java
@@ -1,9 +1,11 @@
 package com.jeniferprochnow.facebookcloneservice;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@EnableJpaRepositories(basePackages = {"com.jeniferprochnow.facebookcloneservice"})
+@EntityScan(basePackages = {"com.jeniferprochnow.facebookcloneservice.entity"})
 public class FacebookCloneServiceApplication {
 
 	public static void main(String[] args) {
